ee13277548b641843777ccb9f1383d5ea52fda96
LinusHolmer <linus.holmer@stud.sti.se>
2025-10-27 11:31:29 +0100

    lektion 6 initial commit

---
+++ lektion6/src/main/java/com/example/lektion6/Lektion6Application.java
@@ -0,0 +1,13 @@
+package com.example.lektion6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion6Application.class, args);
+	}
+
+}
---
+++ lektion6/src/test/java/com/example/lektion6/Lektion6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lektion6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lektion6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ec461d9ee09f8f9d804a1de9681074d32b7d5ff3
LinusHolmer <linus.holmer@stud.sti.se>
2025-10-27 11:55:23 +0100

    Update WebConfig.java

--- lektion6/src/main/java/com/example/lektion6/WebConfig.java
+++ lektion6/src/main/java/com/example/lektion6/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         WebMvcConfigurer.super.addViewControllers(registry);

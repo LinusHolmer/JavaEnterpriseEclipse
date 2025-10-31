0484c5251a3cc3041f585c231cd28b319d935927
LinusHolmer <linus.holmer@stud.sti.se>
2025-10-31 13:39:16 +0100

    Update DebugRestController.java

--- lektion6/src/main/java/com/example/lektion6/DebugRestController.java
+++ lektion6/src/main/java/com/example/lektion6/DebugRestController.java
@@ -1,7 +1,6 @@
 package com.example.lektion6;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
